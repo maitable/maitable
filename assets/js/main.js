4e2eb004ca33be37ef17aee218d5aa872e547ecc
root <root@picoctf>
2026-04-02 23:40:17 +0100

    worked out an about me

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,5 @@ const heading = document.querySelector('h1');
 button.addEventListener('click', () => {
     document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
     heading.textContent = 'Six seven';
-    button.textContent = 'Why would you click this';
+    button.textContent = 'Scroll up';
 });
