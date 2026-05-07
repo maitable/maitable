226184b8932c4b19f22e164f73c4a18e40145952
maitable <maiapricop18@gmail.com>
2026-05-07 14:30:54 +0300

    render stuff from json and add emoji favicon

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.querySelectorAll('a').forEach(a => {
     a.classList.add('active');
   }
 });
+
+fetch('data.json')
+  .then(r => r.json())
+  .then(data => {
+    const key = window.location.pathname.includes('enjoyables') ? 'enjoyables' : 'achievements';
+    const ul = document.querySelector('ul');
+    data[key].forEach(a => {
+      const li = document.createElement('li');
+      li.textContent = a;
+      ul.appendChild(li);
+    });
+  });
